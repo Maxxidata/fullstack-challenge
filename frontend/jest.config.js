ccf1028dab00315a6f873c41266d7237683d1dec
Maycon <mayconddsilva@gmail.com>
2021-03-01 13:30:48 -0300

    chore: add react configs

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}'],
   coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
   transform: { '.+\\.(js|jsx)$': 'babel-jest' }
 }
