64462b5e8539e212468f9e5ac29a8e17faffd04c
Maycon <mayconddsilva@gmail.com>
2021-03-02 20:50:18 -0300

    chore: add axios mock adapter

--- frontend/src/main/index.jsx
+++ frontend/src/main/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import 'antd/dist/antd.css'
+import '../mock'
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ frontend/src/mock/index.js
@@ -0,0 +1,4 @@
+import mock from './mock'
+import './professional'
+
+mock.onAny().passThrough()
