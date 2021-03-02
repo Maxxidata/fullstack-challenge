ce0070bb8fc608504ff4f74e3b4e1eee4e6969a7
Maycon <mayconddsilva@gmail.com>
2021-03-02 16:59:23 -0300

    chore: add testing library.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}'],
   coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/config/jest-setup.js'],
   testEnvironment: 'jsdom',
   transform: { '.+\\.(js|jsx)$': 'babel-jest' },
   moduleNameMapper: {
