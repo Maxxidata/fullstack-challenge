faca5860b20aa974e2486fb41ed599818679b4ec
Maycon <mayconddsilva@gmail.com>
2021-03-02 19:25:28 -0300

    feat: create professional list

--- frontend/src/presentation/pages/professional/professional-list.jsx
+++ frontend/src/presentation/pages/professional/professional-list.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import Content from '../../components/layout/content/content.jsx'
+import DataGrid from '../../components/datagrid/datagrid.jsx'
 
 const ProfessionalList = () => {
-  return <div>Professional</div>
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'id',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: 'Nome',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: 'Telefone',
+        dataIndex: 'phone',
+        key: 'phone'
+      },
+      {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+      },
+      {
+        title: 'Tipo',
+        dataIndex: 'type',
+        key: 'type'
+      },
+      {
+        title: 'Situação',
+        dataIndex: 'status',
+        key: 'status',
+        render: (status) => (status ? 'Ativo' : 'Inativo')
+      }
+    ]
+  })
+
+  return (
+    <Content>
+      <DataGrid
+        columns={columns}
+      />
+    </Content>
+  )
 }
 
 export default ProfessionalList
