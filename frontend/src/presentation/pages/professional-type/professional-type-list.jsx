260f1bf71fffddd669bee712106861970e449bc4
Maycon <mayconddsilva@gmail.com>
2021-03-03 20:28:26 -0300

    feat: create professional type list

--- frontend/src/presentation/pages/professional-type/professional-type-list.jsx
+++ frontend/src/presentation/pages/professional-type/professional-type-list.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Content, DataGrid } from '@/presentation/components'
 
 const ProfessionalTypeList = () => {
-  return <div>Professional Type List</div>
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'id',
+        dataIndex: 'id',
+        key: 'id'
+      },
+      {
+        title: 'Descrição',
+        dataIndex: 'description',
+        key: 'name'
+      },
+      {
+        title: 'Situação',
+        dataIndex: 'status',
+        key: 'status',
+        render: (status) => (status ? 'Ativo' : 'Inativo')
+      }
+    ]
+  })
+
+  return (
+    <Content>
+      <DataGrid
+        path="/professional-types"
+        columns={columns}
+      />
+    </Content>
+  )
 }
 
-export default ProfessionalTypeList
+export default withRouter(ProfessionalTypeList)
