99df70ac1582e646355a739450240167be1e8f8e
Maycon <mayconddsilva@gmail.com>
2021-03-03 20:27:18 -0300

    refactor: remove react strict mode to fix warnings

--- frontend/src/main/index.jsx
+++ frontend/src/main/index.jsx
@@ -4,9 +4,4 @@ import App from './App.jsx'
 import 'antd/dist/antd.css'
 import '../mock'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('main')
-)
+ReactDOM.render(<App />, document.getElementById('main'))
