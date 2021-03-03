674f9eedb45ffb405d350d7c69666784323d02d7
Maycon <mayconddsilva@gmail.com>
2021-03-03 19:57:50 -0300

    test: ensure FormItem show message error

---
+++ frontend/src/presentation/components/form/item/form-item.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import faker from 'faker'
+import { Input } from 'antd'
+import FormItem from './form-item.jsx'
+
+const makeSut = (error) => {
+  render(<FormItem error={error}><Input/></FormItem>)
+}
+
+describe('FormItem', () => {
+  test(('Should show an error message when error was passed'), () => {
+    const error = faker.random.words()
+    makeSut(error)
+    expect(screen.getByText(error)).toBeInTheDocument()
+  })
+})
