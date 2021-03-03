d160a1282df34119bf21be63a590e09646fe8d4d
Maycon <mayconddsilva@gmail.com>
2021-03-03 20:52:42 -0300

    feat: Add get by id, put and post method to professional mock

--- frontend/src/mock/professional-type.js
+++ frontend/src/mock/professional-type.js
@@ -17,3 +17,39 @@ const database = {
 }
 
 mock.onGet('/professional-types').reply(() => [200, { items: database.professionalTypes }])
+
+mock.onGet(/\/professional-types\/\d+/).reply((request) => {
+  const id = request.url.split('/')[2]
+  const professional = database.professionalTypes.find((record) => (record.id).toString() === id)
+
+  if (professional == null) {
+    return [404]
+  }
+
+  return [200, professional]
+})
+
+mock.onPost('/professional-types').reply((request) => {
+  const professional = JSON.parse(request.data)
+  database.professionalTypes.push({ id: database.professionalTypes.length + 1, ...professional })
+  return [200, professional]
+})
+
+mock.onPut(/\/professional-types\/\d+/).reply((request) => {
+  const id = request.url.split('/')[2]
+  const professional = JSON.parse(request.data)
+
+  if (database.professionalTypes.find((record) => record.id === id) == null) {
+    return [404]
+  }
+
+  database.professionalTypes = database.professionalTypes.map((record) => {
+    if (record.id === id) {
+      return professional
+    }
+
+    return record
+  })
+
+  return [200, professional]
+})
