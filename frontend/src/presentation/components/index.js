b92d223cabc436f0c87fc17739996167185ccc5a
Maycon <mayconddsilva@gmail.com>
2021-03-03 20:14:21 -0300

    refactor: Add FormItem component to index file

--- frontend/src/presentation/components/index.js
+++ frontend/src/presentation/components/index.js
@@ -2,3 +2,4 @@ export { default as AutoPaginatedSelect } from './auto-paginated-select/auto-pag
 export { default as DataGrid } from './data-grid/data-grid.jsx'
 export { default as Content } from './layout/content/content.jsx'
 export { default as Select } from './select/select.jsx'
+export { default as FormItem } from './form/item/form-item.jsx'
