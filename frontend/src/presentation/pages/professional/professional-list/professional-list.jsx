8494316162488b8d4de4176213bf797d9679ce08
Maycon <mayconddsilva@gmail.com>
2021-03-03 18:32:13 -0300

    refactor move professional-list component do professional-list folder

---
+++ frontend/src/presentation/pages/professional/professional-list/professional-list.jsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useMemo } from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+import Content from '../../../components/layout/content/content.jsx'
+import DataGrid from '../../../components/datagrid/datagrid.jsx'
+
+const ProfessionalList = ({ history }) => {
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'id',
+        dataIndex: 'id',
+        key: 'id'
+      },
+      {
+        title: 'Nome',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: 'Telefone',
+        dataIndex: 'phone',
+        key: 'phone'
+      },
+      {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+      },
+      {
+        title: 'Tipo',
+        dataIndex: 'professionalType',
+        key: 'professionalType',
+        render: (professionalType) => (professionalType.description)
+      },
+      {
+        title: 'Situação',
+        dataIndex: 'status',
+        key: 'status',
+        render: (status) => (status ? 'Ativo' : 'Inativo')
+      }
+    ]
+  })
+
+  const handleNew = useCallback(() => {
+    history.push('/professionals/form/new')
+  }, [history])
+
+  const handleEdit = useCallback((record) => {
+    history.push(`/professionals/form/${record.id}`)
+  }, [history])
+
+  return (
+    <Content>
+      <DataGrid
+        path="/professionals"
+        columns={columns}
+        onNew={handleNew}
+        onEdit={handleEdit}
+      />
+    </Content>
+  )
+}
+
+ProfessionalList.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func
+  }).isRequired
+}
+export default withRouter(ProfessionalList)
