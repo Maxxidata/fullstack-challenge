ca8a4fcf9adc9ceaa6f321c1fb5f1ac57ddad97e
Maycon <mayconddsilva@gmail.com>
2021-03-03 20:22:18 -0300

    refactor: change professional type database mock to a object with professionalTypes field

--- frontend/src/mock/professional-type.js
+++ frontend/src/mock/professional-type.js
@@ -1,17 +1,19 @@
 import mock from './mock'
 
-const database = [
-  {
-    id: '1',
-    description: 'Médico(a)',
-    status: true
-  },
-  {
-    id: '2',
-    description: 'Professor(a)',
-    status: true
-  }
+const database = {
+  professionalTypes: [
+    {
+      id: '1',
+      description: 'Médico(a)',
+      status: true
+    },
+    {
+      id: '2',
+      description: 'Professor(a)',
+      status: true
+    }
 
-]
+  ]
+}
 
 mock.onGet('/professional-types').reply(() => [200, { items: database }])
