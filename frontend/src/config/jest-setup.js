3f8eb49b02b1a8a65e045bccd126742279f7b825
Maycon <mayconddsilva@gmail.com>
2021-03-03 19:54:22 -0300

    fix: fix jest media problem.

--- frontend/src/config/jest-setup.js
+++ frontend/src/config/jest-setup.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-undef */
 import '@testing-library/jest-dom'
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+})
