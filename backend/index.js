b0bc68de48960718af497d7134d5662cacd134f4
Maycon <mayconddsilva@gmail.com>
2021-03-04 19:49:04 -0300

    feat: change hello world message

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Challenge Backend by Maycon Silva');
 });
 
 app.listen(3000);
