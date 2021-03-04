150ef768e097a15f32ff9cacfcfa20c629d27014
Maycon <mayconddsilva@gmail.com>
2021-03-04 19:47:05 -0300

    feat: create professional type migration.

---
+++ backend/database/migrations/20210304220414-create-professional-type.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("ProfessionalType", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('ProfessionalType');
+  },
+};
