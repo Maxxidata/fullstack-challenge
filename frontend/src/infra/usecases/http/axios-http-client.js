37bbfd0713f8499eed4793096cd5ca383861f9da
Maycon <mayconddsilva@gmail.com>
2021-03-04 12:51:03 -0300

    feat: add env var to API URL

--- frontend/src/infra/usecases/http/axios-http-client.js
+++ frontend/src/infra/usecases/http/axios-http-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosHttpClient = axios.create({
-  baseURL: 'http://localhost::3000/api'
+  baseURL: process.env.API_URL
 })
 
 export default AxiosHttpClient
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -40,6 +41,9 @@ module.exports = {
     'react-dom': 'ReactDOM'
   },
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://localhost::3000/api')
+    })
   ]
 }
