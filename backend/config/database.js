368ebc07a1dc77cd7dabc4ac4ebce4ed8ce5c1ab
Maycon <mayconddsilva@gmail.com>
2021-03-04 18:47:02 -0300

    chore: config sequelize

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+  }
+};
