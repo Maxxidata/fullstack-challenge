c686a9a94a37abcdad6da80a36adfe51f27d4f4e
Maycon <mayconddsilva@gmail.com>
2021-03-04 20:14:34 -0300

    feat: create Professional type model

---
+++ backend/app/models/professionalType.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+  const ProfessionalType = sequelize.define("ProfessionalType", {
+    description: DataTypes.STRING,
+    status: DataTypes.BOOLEAN,
+  });
+
+  return ProfessionalType;
+};
