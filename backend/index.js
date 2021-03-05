4b66d25304d611d9bdcbb000fa3bbd8aafb08c76
Maycon <mayconddsilva@gmail.com>
2021-03-05 12:29:19 -0300

    chore: add cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 
 var professionalRouter = require("./app/routes/professional");
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
