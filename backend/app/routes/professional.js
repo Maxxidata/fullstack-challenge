eafe0d891054041c16330ccc7cb4d38ab919eb64
Maycon <mayconddsilva@gmail.com>
2021-03-05 12:33:52 -0300

    feat: add professional route

--- backend/app/routes/professional.js
+++ backend/app/routes/professional.js
@@ -2,10 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 const ProfessionalType = require("../controllers/ProfessionalType");
+const Professional = require("../controllers/Professional");
 
 router.post("/types", ProfessionalType.create);
 router.get("/types", ProfessionalType.list);
 router.get("/types/:id", ProfessionalType.getById);
 router.put("/types/:id", ProfessionalType.update);
+
+router.post("/professionals", Professional.create);
+router.get("/professionals", Professional.list);
+router.get("/professionals/:id", Professional.getById);
+router.put("/professionals/:id", Professional.update);
 
 module.exports = router;
