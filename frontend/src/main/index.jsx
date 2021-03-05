4e77b67b667ba2bdeadc2db4ac097055b0dbb8f2
Maycon <mayconddsilva@gmail.com>
2021-03-05 12:28:00 -0300

    chore: comment mock importation

--- frontend/src/main/index.jsx
+++ frontend/src/main/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import 'antd/dist/antd.css'
 import '@/main/configs/i18n.js'
-import '../mock'
+// import '../mock'
 
 ReactDOM.render(<App />, document.getElementById('main'))
