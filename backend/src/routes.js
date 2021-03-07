0eddd0df047caa99035b9813024fa838994d08a2
Juan Carlos <juanksoliveira@gmail.com>
2021-03-06 21:21:46 -0300

    style: format all files

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", function(req, res) {
-    res.send({
-        message: "It Works!!! 🎉",
-    });
+router.get("/", function (req, res) {
+  res.send({
+    message: "It Works!!! 🎉",
+  });
 });
 
 module.exports = router;
