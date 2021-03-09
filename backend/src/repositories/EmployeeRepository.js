9c54c2d52b824e4e3910adff5cffcfa439edff51
Juan Carlos <juanksoliveira@gmail.com>
2021-03-09 17:51:17 -0300

    feat(repos): implement Employee Repository

--- backend/src/repositories/EmployeeRepository.js
+++ backend/src/repositories/EmployeeRepository.js
@@ -1,18 +1,48 @@
+const { sequelize } = require("../models/");
+const Employee = require("../models/Employee")(sequelize);
+
 const EmployeeRepository = {
-    async findAll() {
-        console.log("EmployeeRepository findAll");
+    async findAll(page = 1, size = 25) {
+        try {
+            const offset = (page - 1) * size;
+            const limit = size;
+            return await Employee.findAll({ offset, limit });
+        } catch (err) {
+            console.error(err);
+        }
     },
     async findById(id) {
-        console.log("EmployeeRepository findById");
+        try {
+            return await Employee.findByPk(id);
+        } catch (err) {
+            console.error(err);
+        }
     },
     async create(employee) {
-        console.log("EmployeeRepository create");
+        try {
+            return await Employee.create(employee);
+        } catch (err) {
+            console.error(err);
+        }
     },
-    async update(employee) {
-        console.log("EmployeeRepository update");
+    async update(id, employee) {
+        try {
+            return await Employee.update(employee, {
+                where: { id },
+                returning: true,
+            });
+        } catch (err) {
+            console.error(err);
+        }
     },
     async delete(id) {
-        console.log("EmployeeRepository delete");
+        try {
+            return await Employee.destroy({
+                where: { id },
+            });
+        } catch (err) {
+            console.error(err);
+        }
     },
 };
 
