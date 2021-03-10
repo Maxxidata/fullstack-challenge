946bb0a8d86de6a85b702acaa206266d461b8d9e
Franklin <gentilezaxd@gmail.com>
2021-03-10 16:19:19 -0400

    Conclude Professional CRUD

--- backend/src/controllers/ProfessionalsController.js
+++ backend/src/controllers/ProfessionalsController.js
@@ -3,6 +3,22 @@ const TypeOfProfessional = require('../models/TypeOfProfessional')
 
 
 class ProfessionalsController {
+    async index(request, response){
+        const { id } = request.params;
+        
+        const professional = await Professional.findByPk(id, {
+            include: { association: 'type', attributes: ['description'] }
+        });
+
+        return response.status(200).json(professional)
+    }
+    async show(request, response){
+        const professionals = await Professional.findAll({
+            include: { association: 'type', attributes: ['description'] }
+        });
+
+        return response.status(200).json(professionals)
+    }
     async create(request, response){
         const { name, email, phone, situation, type_of_professional } = request.body
 
@@ -16,6 +32,37 @@ class ProfessionalsController {
         })
 
         return response.status(200).json(profissional)
+    }
+    async update(request, response){
+        const { id } = request.params;
+        const { name, email, phone, situation, type_of_professional } = request.body;
+
+        let professional = await Professional.findByPk(id);
+
+        if (!professional) return response.status(404).json()
+
+        const description = type_of_professional.toUpperCase();
+        const [ typeOfProfessional ] = await TypeOfProfessional.findOrCreate({
+            where: { description }
+        })
+
+        const updatedProfessional = await professional.update({
+            name, email, phone, situation, type_of_professional: typeOfProfessional.dataValues.id
+        })
+
+        return response.status(200).json(updatedProfessional)
+    }
+    async destroy(request, response){
+        const { id } = request.params
+
+        const professional = await Professional.findByPk(id);
+
+        if(!professional) return response.status(404)
+
+        console.log(professional)
+        await professional.destroy();
+
+        return response.status(204).json()
     }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,5 +6,9 @@ const professionalsController = new ProfessionalsController();
 const routes = express.Router()
 
 routes.post('/professionals', professionalsController.create)
+routes.get('/professionals', professionalsController.show)
+routes.get('/professionals/:id', professionalsController.index)
+routes.put('/professionals/:id', professionalsController.update)
+routes.delete('/professionals/:id', professionalsController.destroy)
 
 module.exports = routes;
