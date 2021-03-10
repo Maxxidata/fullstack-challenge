c8b50bd138c811afecf54cb4472f29fd561a8753
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2021-03-10 20:34:57 -0300

    fix: using api base url from .env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/api'
+  baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
 export default api;
