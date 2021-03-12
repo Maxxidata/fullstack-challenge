05d6dbf643f89e58e69067ea41083b70f19877d5
Franklin <gentilezaxd@gmail.com>
2021-03-12 14:20:38 -0400

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const routes = require('./routes')
-
+const cors = require('cors')
 require('./database')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/',routes);
 
