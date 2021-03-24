b0b6df453ddd59c6c96fdc30ff4fa5ff6580f5a8
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2021-03-24 20:31:48 -0300

    feat: improve professional type controller

--- backend/src/app/controllers/ProfessionalTypeController.js
+++ backend/src/app/controllers/ProfessionalTypeController.js
@@ -1,21 +1,38 @@
+import ProfessionalTypeService from '../services/ProfessionalTypeService';
+
 class ProfessionalTypeController {
-  async list(req, res) {
-    return;
-  }
-
-  async getOne(req, res) {
-    return;
-  }
-
-  async create(req, res) {
-    return;
+  constructor() {
+    this.professionalTypeService = ProfessionalTypeService;
   }
   
-  async update(req, res) {
-    return;
+  async list() {
+    return await this.professionalTypeService.list();
   }
 
-  async delete(req, res) {
+  async getOne(id) {
+    const professionalType = await this.professionalTypeService.getOne(id);
+
+    if (!professionalType) {
+      throw "Usuário não encontrado!";
+    }
+
+    return professionalType;
+  }
+
+  async create(data) {
+    const professionalType = await this.professionalTypeService.create(data);
+    return professionalType;
+  }
+  
+  async update(id, data) {
+    const professionalType = await this.professionalTypeService.update(id, data);
+    return professionalType;
+  }
+
+  async delete(id) {
+    await this.professionalTypeService.delete(id);
     return;
   }
 }
+
+export default new ProfessionalTypeController;
